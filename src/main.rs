af396abfd95964473ef819d4826108c0575c93b5
joffreybesos <93067706+joffreybesos@users.noreply.github.com>
2022-08-19 21:03:28 +1000

    adding some logging

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,15 @@ mod generate;
 mod cache;
 
 fn main() {
-    let seed = "6344463";
+    let seed = "63444463";
     let difficulty = "2";
     let cached_seed_data_file = cache::cached_file_name(seed, difficulty);
     let seed_data: String = if Path::new(&cached_seed_data_file).exists() {
+        println!("Reading cached map data from file {}", &cached_seed_data_file.to_str().unwrap());
         cache::read_cached_file(cached_seed_data_file)
     } else {
+        println!("Generating fresh data for seed {} and difficulty {}", seed, difficulty);
         generate::generate_data(seed, difficulty)
     };
-    println!("{}", &seed_data);
+    // println!("{}", &seed_data);
 }
